3b5e32bac345199163cdcb77738d8fcb647cff58
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2024-05-08 09:00:29 +0200

    chore: Add button to load readers and books from API

--- RxBookTracker/index.ts
+++ RxBookTracker/index.ts
@@ -1,12 +1,14 @@
-import { fromEvent, Observable } from "rxjs";
+import { concat, fromEvent, Observable } from "rxjs";
 import { allBooks, allReaders, type Book, type Reader } from "./data";
 import { fromFetch } from 'rxjs/fetch';
 import { ajax } from 'rxjs/ajax';
 
 const showReadersButton = document.getElementById('showReaders');
 const loadReadersButton = document.getElementById('loadReaders');
+const loadReadersAndBooksButton = document.getElementById('loadReadersAndBooks');
 const readersDiv = document.getElementById('readers');
 const loadedReadersDiv = document.getElementById('loadedReaders');
+const loadedReadersAndBooksDiv = document.getElementById('loadedReadersAndBooks');
 
 const allBooksObservable$ = new Observable(subscriber => {
     for (let book of allBooks) {
@@ -35,10 +37,11 @@ const onShowReaders = () => {
 fromEvent(showReadersButton, 'click').subscribe(onShowReaders);
 
 //data from API call
+const readers$ = fromFetch('http://localhost:3000/api/readers')
+const books$ = fromFetch('http://localhost:3000/api/books')
 
 const onLoadReaders = () => {
-    const data$ = fromFetch('http://localhost:3000/api/readers')
-    data$.subscribe({
+    readers$.subscribe({
         next: response => {
             loadedReadersDiv.innerHTML = '';
             response.json().then((data) => {
@@ -52,6 +55,22 @@ const onLoadReaders = () => {
         complete: () => console.log('done')
     });
 }
+const onLoadReaderAndBook = () => {
+    concat(readers$, books$).subscribe({
+        next: response => {
+            loadedReadersAndBooksDiv.innerHTML = '';
+            response.json().then((data) => {
+                data.forEach((readerAndBooks: Reader & Book) => {
+                    const newDiv = document.createElement('div');
+                    newDiv.innerText = readerAndBooks.name || readerAndBooks.title;
+                    loadedReadersAndBooksDiv.appendChild(newDiv);
+                })
+            })
+        },
+        complete: () => console.log('done')
+    });
+}
 
 fromEvent(loadReadersButton, 'click').subscribe(onLoadReaders);
+fromEvent(loadReadersAndBooksButton, 'click').subscribe(onLoadReaderAndBook);
 
--- RxBookTracker/server.js
+++ RxBookTracker/server.js
@@ -22,12 +22,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // app.use('/', routes);
-app.use('/api/readers', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    return readers(req, res, next);
-});
+app.use('/api/readers', readers);
 app.use('/api/books', books);
 app.use('/api/errors', errors);
 app.get('*', function(req, res) {
