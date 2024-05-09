7ba38144d7f17fdbb4442c62f0dbfa5d1abf059c
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2024-05-09 22:25:27 +0200

    chore: Add RxJS subscriptions and observable for fruit data

--- SignalsRxJSBasics/src/app/app.component.ts
+++ SignalsRxJSBasics/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { from, fromEvent, map, Observable, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,41 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent implements OnInit, OnDestroy {
   name = 'Angular';
+  ofSub!: Subscription;
+  fromSub!: Subscription;
+  buttonSubscriptions!: Subscription;
+
+  fruits = ''
+  fruitObservable!: Observable<string>;
 
   ngOnInit() {
     console.log('AppComponent initialized');
+    this.ofSub = of(2,4,6,8).subscribe((item: number) => console.log('of item:', item));
+    this.fromSub = from([2,4,6,8]).subscribe((item: number) => console.log('from item:', item));
+    this.fruitObservable = from(['apple', 'orange', 'banana', 'kiwi']).pipe(
+      map(fruit => {
+        const attributed = ['fresh', 'sweet', 'delicious'];
+        const randomAttribute = attributed[Math.floor(Math.random() * attributed.length)];
+        return `${[randomAttribute]} ${fruit.toUpperCase()}`
+      })
+      
+    );
+  }
+
+  onClick = () => {
+    this.fruits = '';
+    this.fruitObservable.subscribe((fruit) => {
+      this.fruits += fruit + ', ';
+    })
+  
   }
 
   ngOnDestroy() {
     console.log('AppComponent destroyed');
+
+    this.ofSub.unsubscribe();
+    this.fromSub.unsubscribe();
+    this.buttonSubscriptions.unsubscribe();
   }
 
 }
